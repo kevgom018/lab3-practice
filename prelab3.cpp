76416d2e98ebf4e91ff31e3ad2116d2efc6218c2
kevgom018 <kevin.gomez6@upr.edu>
2024-02-08 14:35:37 -0400

    Defined the assigned switchDayName fucntion and tested it.

---
+++ prelab3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string switchDayName(int num);
+
+int main(){
+    cout << switchDayName(9) << endl;
+    cout << switchDayName(2) << endl;
+    return 0;
+}
+
+string switchDayName(int num){
+    switch(num){
+        case 1:
+        return "Sunday";
+        case 2:
+        return "Monday";
+        case 3:
+        return "Tuesday";
+        case 4:
+        return "Wednesday";
+        case 5:
+        return "Thursday";
+        case 6:
+        return "Friday";
+        case 7:
+        return "Saturday";
+        default:
+        return "Invalid number";
+    }
+}
